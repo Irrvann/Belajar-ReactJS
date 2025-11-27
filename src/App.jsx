4ab6d999609e21ca6ab8f9d09a35151ed3be8ead
himpaudikedungwuni-31 <himpaudikedungwuni31@gmail.com>
2025-11-27 19:23:50 +0700

    inisialisasi projek reactJS

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Button from './components/Elements/Button';
+
+
+
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <Button />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Elements/Button/Index.jsx
@@ -0,0 +1,9 @@
+const Button = () => {
+    return (
+        <button className="bg-blue-500 text-white px-4 py-2 rounded font-semibold hover:bg-blue-600 rounded-lg shadow-md transition duration-300">
+            Login
+        </button>
+    );
+}
+
+export default Button;
